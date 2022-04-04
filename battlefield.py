0159c77d693fd38c1dc97edc74095fa2f15fff29
Robert Gessler <ragessler@roberts-mbp.lan>
2022-04-04 10:02:01 -0500

    added weapon file and Weapon class

---
+++ battlefield.py
@@ -0,0 +1,13 @@
+class Battlefield:
+    def __init__(self):
+        self.robot = robot
+        self.dinosaur = dinosaur
+    def run_game(self):
+        pass
+    def display_welcome(self):
+        pass
+    def battle_phase(self):
+        pass
+    def display_winner(self):
+        pass
+    
---
+++ robot.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from xml.dom.domreg import well_known_implementations
+
+
+class Robot:
+    def __init__(self):
+        self.name = ''
+        self.health = 0
+        self.active_weapon = weapon 
+    def attact(self, dinosaur):
+        pass
